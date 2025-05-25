95225e3fb9d0c7f46a3fa67c6176d7ce229cb50b
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-05-25 11:33:30 +0800

    feat: update @tanstack/react-query-devtools to version 5.77.0 and add suppressHydrationWarning to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 
-import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
@@ -21,7 +20,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${geist.variable}`} suppressHydrationWarning>
       <body>
         <TRPCReactProvider>
           <Providers>
