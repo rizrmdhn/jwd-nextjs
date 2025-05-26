52bab8faff0434402f9eb4211104f7cc949c0cac
rizrmdhn <rizrmdhn.work@gmail.com>
2025-05-26 10:57:51 +0800

    feat: implement database seeding for products in seeders

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -1,7 +1,14 @@
+import { seed } from "drizzle-seed";
 import seedUsers from "./users";
+import { db } from "@/server/db";
+import { products } from "@/server/db/schema";
 
 async function seeders() {
   await seedUsers();
+
+  await seed(db, products, {
+    count: 15,
+  });
 }
 
 seeders()
